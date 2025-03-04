ea0cd2adfce0095cd704c8dc44cbaaffc98666a6
Konstantin Voronin <heavensmiting@gmail.com>
2025-03-04 03:19:33 +0300

    импорт функций в исходный модуль для проверки работоспособности

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card, get_date
 
 # Данные от пользователя
-user_card = input("Введите номер карты: ")
-user_account = input("Введите номер счета: ")
+users_number_card = input("Введите номер карты: ")
+users_account_card = input("Введите номер счета: ")
+date_input = str(input("Введите дату: "))
 
-result_card = get_mask_card_number(user_card)
-result_account = get_mask_account(user_account)
+result_numbers = mask_account_card(users_number_card)
+result_account = mask_account_card(users_account_card)
+result = get_date(date_input)
 
-print(result_card)
+print(result_numbers)
 print(result_account)
+print(result)
+
+
+
