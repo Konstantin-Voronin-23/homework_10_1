df51082b1fb2d5ecf0f6109ab2b85502159f309b
Konstantin Voronin <heavensmiting@gmail.com>
2025-04-01 20:52:10 +0300

    Fix

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,10 +3,10 @@ import sys
 import pytest
 from loguru import logger
 
-from src.decorators import log
+from src.decorators import log, my_function
 
 
-def test_my_function_valid(capsys: pytest.CaptureFixture) -> None:
+def test_log_valid(capsys: pytest.CaptureFixture) -> None:
     """Тест проверяет корректность стандартных операций"""
     logger.remove()
     logger.add(sys.stdout)
@@ -24,7 +24,7 @@ def test_my_function_valid(capsys: pytest.CaptureFixture) -> None:
     assert "my_function | ok" in captured.out
 
 
-def test_my_function_invalid(capsys: pytest.CaptureFixture) -> None:
+def test_log_invalid(capsys: pytest.CaptureFixture) -> None:
     """Тест проверяет корректность нестандартных операций в данном примере с отрицательными значениями"""
     logger.remove()
     logger.add(sys.stdout)
@@ -42,7 +42,7 @@ def test_my_function_invalid(capsys: pytest.CaptureFixture) -> None:
     assert "my_function | ok" in captured.out
 
 
-def test_my_function_raise(capsys: pytest.CaptureFixture) -> None:
+def test_log_raise(capsys: pytest.CaptureFixture) -> None:
     """Тест проверяет корректную отработку исключений в данном примере при делении на 0"""
     logger.remove()
     logger.add(sys.stdout)
@@ -57,3 +57,19 @@ def test_my_function_raise(capsys: pytest.CaptureFixture) -> None:
         my_function(100, 0)
     captured = capsys.readouterr()
     assert "my_function error: | ZeroDivisionError" in captured.out
+
+
+def test_my_function_valid() -> None:
+    """Тест для проверки положительных чисел"""
+    assert my_function(2, 1) == 2.0
+
+
+def test_my_function_invalid() -> None:
+    """Тест для проверки отрицательных чисел"""
+    assert my_function(-500, 20) == -25.0
+
+
+def test_my_function_raise() -> None:
+    """Тест для проверки исключений функции"""
+    with pytest.raises(ZeroDivisionError):
+        my_function(359, 0)
