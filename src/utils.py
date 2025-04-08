d6e6d6bbe3b8e832c70cd42cde12f5c1265a5fce
Konstantin Voronin <heavensmiting@gmail.com>
2025-04-09 02:35:01 +0300

    Добавлен json файл operations.json который содержит все операции в новой директории data, добавлен новый модуль utils в котором реализована функция чтения json файла с операциями и отработкой исключений

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import json
+
+def read_json_file(file_path="operations.json"):
+    """Функция для чтения json файла с операциями транзакций"""
+    try:
+        with open("operations.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError as error:
+        print(f"Ошибка: файл {file_path} не найден! ")
+        return None
+    except json.JSONDecodeError:
+        print(f"Ошибка: файл {file_path} содержит некорректный JSON! ")
+        return None
