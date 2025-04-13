7e121c5414b5c67dbb8f915aaa2cf6d44f8d53fc
Konstantin Voronin <heavensmiting@gmail.com>
2025-04-14 02:58:22 +0300

    реализована функция read_csv_file для чтения csv файла

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from src.widget import get_date, mask_account_card
+from src.csv_excel_file_reader import read_csv_file
+from config import PATH_TO_CSV
+from pprint import pprint
 
-# Данные от пользователя
-users_number_card = input("Введите номер карты: ")
-users_account_card = input("Введите номер счета: ")
-date_input = str(input("Введите дату: "))
 
-result_numbers = mask_account_card(users_number_card)
-result_account = mask_account_card(users_account_card)
-result_date = get_date(date_input)
-
-print(result_numbers)
-print(result_account)
-print(result_date)
+if __name__=="__main__":
+    pprint(read_csv_file(PATH_TO_CSV))
--- src/csv_excel_file_reader.py
+++ src/csv_excel_file_reader.py
@@ -1,10 +1,30 @@
 import pandas as pd
 import csv
+from typing import Union, List, Dict
 
 
-def read_csv_file():
-    pass
+def read_csv_file(file_path: str, delimiter: str = ";") -> List[Dict[str, Union[str, int, float]]]:
+    """Читает CSV-файл.
+    Принимает на вход путь к csv файлу и разделитель по умолчанию запятая.
+    Возвращает список словарей, где ключ строка, а значение строка | целое число | дробное число """
+
+    if not file_path:
+        raise ValueError("Нужно указать путь к файлу")
+    result = []
+
+    try:
+        with open(file_path, "r", newline="", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=delimiter)
+            for row in reader:
+                try:
+                    print(row)
+                except (ValueError, KeyError) as error:
+                    print(f"Ошибка обработки строки{row}: {error}")
+    except FileNotFoundError as error:
+        print(f"Файл не найден {error}")
+    except UnicodeDecodeError as error:
+        print(f"Ошибка декодирования файла, попробуйте другую кодировку {error}")
 
 
-def read_excel_file():
+def read_excel_file(file_path: str):
     pass
