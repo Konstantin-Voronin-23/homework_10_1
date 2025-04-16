1d27721494712cd622bdb9fe07722f1e3b576fae
Konstantin Voronin <heavensmiting@gmail.com>
2025-04-16 21:36:49 +0300

    fixed

--- src/filter_by_transactions.py
+++ src/filter_by_transactions.py
@@ -1,43 +1,41 @@
-import json
 import re
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 
-def get_filter_by_string(file_path: str, search_string: str) -> List[Dict[str, Any]]:
-    """Функция которая принимает на вход список словарей из json файла и строку поиска, а на выход отдает
-    список словарей у которых в описании есть данныя строка"""
+def get_filter_by_string(transactions: List[Dict[str, Any]], search_string: str) -> List[Dict[str, Any]]:
+    """Функция, которая принимает на вход список словарей и строку поиска, а на выход отдает
+    список словарей, у которых в описании есть данная строка (без учета регистра)."""
 
     if not isinstance(search_string, str):
         raise ValueError("Строка поиска должна быть текстом")
     if not search_string.strip():
         raise ValueError("Нужно ввести текст для сортировки списка транзакций")
+    if not isinstance(transactions, list):
+        raise ValueError("Первый аргумент должен быть списком словарей")
 
     try:
-        with open(file_path, "r", encoding="utf-8") as json_file:
-            transaction = json.load(json_file)
-    except FileNotFoundError as error:
-        raise FileNotFoundError(f"Файл не найден {file_path}") from error
-    try:
-        return [operation for operation in transaction
-                if operation.get("description")
-                and re.search(search_string, operation.get('description'), re.IGNORECASE)]
+        filtered_transactions: List[Dict[str, Any]] = []
+        for operation in transactions:
+            description: Optional[str] = operation.get("description")
+            if description and isinstance(description, str):
+                if re.search(search_string, description, re.IGNORECASE):
+                    filtered_transactions.append(operation)
+        return filtered_transactions
     except Exception as error:
-        raise RuntimeError(f"В процессе произошла ошибка {error}")
+        raise RuntimeError(f"В процессе произошла ошибка: {error}")
 
 
-def get_filter_by_category(file_path: str, categories: list[str]) -> Dict[str, Any]:
-    """Функция которая принимает список словарей из json файла и список категорий, а возвращает словарь
+def get_filter_by_category(transactions: List[Dict[str, Any]], categories: List[str]) -> Dict[str, Any]:
+    """Функция которая принимает список словарей и список категорий, а возвращает словарь
     в котором ключи — это названия категорий, а значения — это количество операций в каждой категории"""
 
     if not isinstance(categories, list):
         raise ValueError("Категории должны быть списком")
     if not categories:
         raise ValueError("Список категорий не может быть пустым")
-    try:
-        with open(file_path, "r", encoding="utf-8") as json_file:
-            transactions = json.load(json_file)
-    except FileNotFoundError as error:
-        raise FileNotFoundError(f"Файл не найден {file_path}") from error
+    if not isinstance(transactions, list):
+        raise ValueError("Первый аргумент должен быть списком словарей")
+
     try:
         result_dict = {category: 0 for category in categories}
         for transaction in transactions:
