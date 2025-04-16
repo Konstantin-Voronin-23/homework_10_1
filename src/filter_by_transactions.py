720a5a48c72e94e43120f095cf578328ecc37741
Konstantin Voronin <heavensmiting@gmail.com>
2025-04-16 20:19:46 +0300

    реализована функция get_filter_by_category которая сортирует список словарей по категориям

--- src/filter_by_transactions.py
+++ src/filter_by_transactions.py
@@ -1,6 +1,7 @@
 import re
 import json
 from typing import List, Dict, Any
+from config import PATH_TO_JSON
 
 
 def get_filter_by_string(file_path: str, search_string: str) -> List[Dict[str, Any]]:
@@ -21,5 +22,31 @@ def get_filter_by_string(file_path: str, search_string: str) -> List[Dict[str, A
         return [operation for operation in transaction
                 if operation.get("description")
                 and re.search(search_string, operation.get('description'), re.IGNORECASE)]
+    except Exception as error:
+        raise RuntimeError(f"В процессе произошла ошибка {error}")
+
+
+# Напишите функцию, которая будет принимать список словарей с данными о банковских операциях и список категорий операций,
+# а возвращать словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
+# Категории операций хранятся в поле description
+def get_filter_by_category(file_path: str, categories: list[str]) -> Dict[str, Any]:
+
+    if not isinstance(categories, list):
+        raise ValueError("Категории должны быть списком")
+    if not categories:
+        raise ValueError("Список категорий не может быть пустым")
+    try:
+        with open(file_path, "r", encoding="utf-8") as json_file:
+            transactions = json.load(json_file)
+    except FileNotFoundError as error:
+        raise FileNotFoundError(f"Файл не найден {file_path}") from error
+    try:
+        result_dict = {category: 0 for category in categories}
+        for transaction in transactions:
+            description = transaction.get('description', '')
+            for category in categories:
+                if category.lower() in description.lower():
+                    result_dict[category] += 1
+        return result_dict
     except Exception as error:
         raise RuntimeError(f"В процессе произошла ошибка {error}")
