aa4b0bd283d6eb64c8a49504a6201c9ca3967e1b
Konstantin Voronin <heavensmiting@gmail.com>
2025-04-16 20:24:04 +0300

    fix by lint

--- src/filter_by_transactions.py
+++ src/filter_by_transactions.py
@@ -1,12 +1,11 @@
-import re
 import json
-from typing import List, Dict, Any
-from config import PATH_TO_JSON
+import re
+from typing import Any, Dict, List
 
 
 def get_filter_by_string(file_path: str, search_string: str) -> List[Dict[str, Any]]:
-    """Функция которая принимает на вход список словарей и строку поиска, а на выход отдает список словарей
-    у которых в описании есть данныя строка"""
+    """Функция которая принимает на вход список словарей из json файла и строку поиска, а на выход отдает
+    список словарей у которых в описании есть данныя строка"""
 
     if not isinstance(search_string, str):
         raise ValueError("Строка поиска должна быть текстом")
@@ -26,10 +25,9 @@ def get_filter_by_string(file_path: str, search_string: str) -> List[Dict[str, A
         raise RuntimeError(f"В процессе произошла ошибка {error}")
 
 
-# Напишите функцию, которая будет принимать список словарей с данными о банковских операциях и список категорий операций,
-# а возвращать словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
-# Категории операций хранятся в поле description
 def get_filter_by_category(file_path: str, categories: list[str]) -> Dict[str, Any]:
+    """Функция которая принимает список словарей из json файла и список категорий, а возвращает словарь
+    в котором ключи — это названия категорий, а значения — это количество операций в каждой категории"""
 
     if not isinstance(categories, list):
         raise ValueError("Категории должны быть списком")
